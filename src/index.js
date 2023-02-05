b05e5d03213584a50d0879080eff4514b4b194d2
Efren Ruiz <Efren.BR2E@gmail.com>
2023-02-05 12:31:33 -0700

    Ventana contacto whatsapp

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import { header } from './modulos/header';
 header();
+
+
+
+const ventanaFondo = document.getElementById('ventanaFondo');
+const ventanaWhatsapp = document.getElementById('ventanaContactoWhatsapp');
+
+const btnWhatsapp = document.getElementById('whatsapp');
+const btnFacebook = document.getElementById('facebook');
+const btnGmail = document.getElementById('gmail');
+
+const btnCerrarWhatsapp = document.getElementById('btnCloseWhatsapp');
+
+
+function cerrarVentanaWhatsapp() {
+    ventanaWhatsapp.classList.add('inactive');
+    ventanaFondo.classList.add('inactive');
+}
+
+function abrirVentanaWhatsapp() {
+    ventanaWhatsapp.classList.remove('inactive');
+    ventanaFondo.classList.remove('inactive');
+}
+
+btnCerrarWhatsapp.addEventListener('click', cerrarVentanaWhatsapp, false);
+btnWhatsapp.addEventListener('click', abrirVentanaWhatsapp, false);
+
+ventanaFondo.addEventListener('click', () => {
+    ventanaWhatsapp.classList.add('inactive');
+    ventanaFondo.classList.add('inactive');
+}, false);
+
